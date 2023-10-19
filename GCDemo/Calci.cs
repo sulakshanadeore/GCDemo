e038a10a981853c015c7f97d39b88100a5a93a3d
deoresulakshana <deoresulakshana@gmail.com>
2023-10-19 09:59:18 +0530

    Add project files.

---
+++ GCDemo/Calci.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCDemo
+{
+    internal class Calci
+    {
+
+        //~Calci() {
+        //    Console.WriteLine("Destructor called.... ");
+        //}
+
+        //When finalize called ie. when destructor is called, finalize method is called, which is overriden and confirms that the object will surely be garbage collected.
+        
+        //suppress finalize means cancelling  the call of finalize is suppress finalize
+
+
+
+        //public override void Finalize()
+        //{
+        //    base.Finalize();
+        //}
+
+        public int add(int i,int j)
+        {
+
+            return i + j;
+        }
+        public int multi(int i, int j)
+        {
+
+            return i * j;
+        }
+    }
+}
---
+++ GCDemo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCDemo
+{
+    public class Demo1
+    {
+        public void Display()
+        {
+            Console.WriteLine("Display called....");
+        }
+    
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Firstexample();
+            Calci cobj = new Calci();
+            Console.WriteLine("Generation of calci " + GC.GetGeneration(cobj));
+            Console.WriteLine("0th Genration  count= " + GC.CollectionCount(0));
+
+            
+            Console.WriteLine("1st Genration  count= " + GC.CollectionCount(1));
+
+            Console.WriteLine("2nd Genration  count= " + GC.CollectionCount(2));
+
+            GC.Collect(0);
+            Console.WriteLine("0th Genration  count= " + GC.CollectionCount(0));
+
+
+
+            Console.ReadLine();
+
+        }
+
+        private static void Firstexample()
+        {
+            //Console.WriteLine(GC.MaxGeneration);
+
+            Console.WriteLine("Total Memory " + GC.GetTotalMemory(false));
+            Demo1 obj = new Demo1();
+            Console.WriteLine("Generation of obj object= " + GC.GetGeneration(obj));
+            Console.WriteLine("Total Memory " + GC.GetTotalMemory(false));
+        }
+    }
+}
